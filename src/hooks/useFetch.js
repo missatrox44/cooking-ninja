5fed6bcdeb29a2072b2af6552279cf92946a8697
Sara Baqla <missatrox44@gmail.com>
2022-12-04 21:56:22 -0600

    outputs recipe titles on homepage

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react"
+
+//accept url into the hook which we want to fetch data from
+export const useFetch = (url) => {
+  //set up state for the data, isPending and error
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    //perform fetch inside useEffect hook whenever the url changes and once initially
+    const fetchData = async () => {
+      setIsPending(true)
+
+      try {
+        const res = await fetch(url, { signal: controller.signal })
+        if (!res.ok) {
+          throw new Error(res.statusText)
+        }
+        const data = await res.json()
+
+        setIsPending(false)
+        setData(data)
+        setError(null)
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("the fetch was aborted")
+        } else {
+          setIsPending(false)
+          setError('Could not fetch the data')
+        }
+      }
+    }
+
+    fetchData()
+
+    return () => {
+      controller.abort()
+    }
+
+  }, [url])
+
+  //return states
+  return { data, isPending, error }
+}
